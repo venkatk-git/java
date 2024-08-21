b16d7212c76bb722f4f3db7d6da6c29404b17fb1
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-21 11:04:16 +0530

    Comparable Main.java

--- Collections/Sorting/Comparable/Main.java
+++ Collections/Sorting/Comparable/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        SortedSet<Integer> treeSet = new TreeSet(new CC());
+        SortedSet<Integer> treeSet = new TreeSet<Integer>(new CC());
         treeSet.add(5);
         treeSet.add(5);
         treeSet.add(0);
@@ -18,9 +18,10 @@ class Main{
 class CC implements Comparator<Integer> {
     @Override
     public int compare(Integer i1, Integer i2) {
-        if(i1 == i2)
+        //This will allow duplicates
+        if(i1 - i2 == 0)
             return 1;
-            
+
         return i2 - i1;
     }
 }
